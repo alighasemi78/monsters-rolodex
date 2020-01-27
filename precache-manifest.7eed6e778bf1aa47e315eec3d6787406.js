57b37519f986c55d423c9d08ded8323a29b21eeb
hesan <hesanghasemi99@gmail.com>
2020-01-27 13:07:43 +0330

    Updates

---
+++ precache-manifest.7eed6e778bf1aa47e315eec3d6787406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb193779a19ce0332f2ecf068f64696b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "044b545d7c9fc379d706",
+    "url": "/monsters-rolodex/static/css/main.1c63d7da.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "044b545d7c9fc379d706",
+    "url": "/monsters-rolodex/static/js/main.8d27ff46.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
